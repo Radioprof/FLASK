1eb288d7ce223952861c7e13889128495e5d2044
Sergey <so.bunin@gmail.com>
2023-02-28 20:53:30 +0300

    Домашнее задание №2

--- blog/app.py
+++ blog/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from blog.article.views import article
 from blog.report.views import report
 from blog.user.views import user
 
@@ -13,4 +14,4 @@ def create_app() -> Flask:
 def register_blueprint(app: Flask):
     app.register_blueprint(user)
     app.register_blueprint(report)
-
+    app.register_blueprint(article)
--- blog/user/views.py
+++ blog/user/views.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, render_template, redirect
-from werkzeug.exceptions import NotFound
 
 user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
 
-# USERS = ['Alice', 'Jon', 'Mike']
 USERS = {
     1: 'Alice',
     2: 'Jon',
@@ -24,7 +22,6 @@ def get_user(pk: int):
     try:
         user_name = USERS[pk]
     except KeyError:
-        # raise NotFound(f'User id {pk} not found')
         return redirect('/users/')
     return render_template(
         'users/details.html',
